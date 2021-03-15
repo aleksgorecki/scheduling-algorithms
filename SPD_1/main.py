1c38222cf8adebf247626cfde6e6694aa8b3881c
Aleksy <goreckialeksander9@gmail.com>
2021-03-15 23:28:02 +0100

    Działanie na arrayach z numpy

--- SPD_1/main.py
+++ SPD_1/main.py
@@ -1,63 +1,74 @@
 import typing
+import numpy as np
 
 
-# struktura przechowujaca dane o zestawie danych wykorzystywanych do testowania algorytmow szeregowania zadan
+# struktura przechowująca dane o zestawie danych wykorzystywanych do testowania algorytmów szeregowania zadań
 class SchedulingData:
     name: str  # nazwa zestawu
-    n_jobs: int  # ilosc zadan
-    n_machines: int  # ilosc maszyn
-    execution_time: typing.List[typing.List[int]]  # macierz o wymiarach n_jobs x n_machines,
-    # zawiera czasy wykonania zadan na maszynach
+    n_jobs: int  # ilość zadan
+    n_machines: int  # ilość maszyn
+    t_matrix: np.array  # macierz o wymiarach n_jobs x n_machines,
+    # zawiera czasy wykonania zadań na maszynach
+    schedule: np.array = None
 
-    def __init__(self, name: str, n_jobs: int, n_machines: int, execution_time: typing.List[typing.List[int]]):
+    def __init__(self, name: str, n_jobs: int, n_machines: int, t_matrix: np.array, schedule: np.array = None):
         self.name = name
         self.n_jobs = n_jobs
         self.n_machines = n_machines
-        self.execution_time = execution_time
+        self.t_matrix = t_matrix
+        if schedule is not None:
+            self.schedule = schedule
 
     def __str__(self):
-        execution_time_str = ""
-        for row in self.execution_time:
-            for column in row:
-                execution_time_str = execution_time_str + str(column) + " "
-            execution_time_str = execution_time_str + "\n"
-        return self.name + str(self.n_jobs) + " " + str(self.n_machines) + execution_time_str
+        t_matrix_str = "\n"
+        for row in range(0, self.n_jobs, 1):
+            for column in range(0, self.n_machines-1, 1):
+                t_matrix_str = t_matrix_str + str(int(self.t_matrix[row][column])) + " "
+            t_matrix_str = t_matrix_str + str(int(self.t_matrix[row][self.n_machines-1])) + "\n"
+        return self.name + str(int(self.n_jobs)) + " " + str(int(self.n_machines)) + t_matrix_str
 
     def __repr__(self):
         return str(self)
 
 
 def read_scheduling_data_file(filename: str, n_sets: int) -> typing.List[SchedulingData]:
-    file_iter = open(filename)
-    read_data_list = []  # lista do ktorej beda dodawane wczytywane zestawy danych
-    sets_read = 0  # licznik wczytanych zestawow
-    while sets_read != n_sets:  # dopoki nie wczytano
-        line = file_iter.readline()  # wczytany wiersz
-        if not line:  # jesli nie udaje sie dalej wczytac wieszy, przerwij petle (np. EOF)
+    file = open(filename)
+    ret = []  # lista do której będą dodawane wczytywane zestawy danych
+    sets_read = 0  # licznik wczytanych zestawów
+    while sets_read != n_sets:  # dopóki nie wczytano
+        line = file.readline()  # wczytany wiersz
+        if not line:  # jeśli nie udaje sie dalej wczytać wieszy, przerwij pętlę (np. EOF)
             break
-        if line[0:4] == "data":  # jesli linia zaczyna sie od 'data' to rozpoznano poczatek zestawu
+        if line[0:4] == "data":  # jeśli linia zaczyna sie od 'data' to rozpoznano początek zestawu
             name = line
-            [n_jobs, n_machines] = [int(item) for item in file_iter.readline().split(' ')]  # konwersja na inty 2 wpisow
-            execution_time = [[]]
+            [n_jobs, n_machines] = [int(item) for item in file.readline().split(' ')]  # konwersja na inty 2 wpisów
+            t_matrix = np.empty(shape=(n_jobs, n_machines))
             for row in range(0, n_jobs, 1):
-                # konwersja na inty wiersza czasow
-                execution_time.append([int(column) for column in file_iter.readline().split(' ')])
-            sets_read = sets_read + 1  # inkrementacja licznika wczytanych zestawow
-            read_data_list.append(SchedulingData(name, n_jobs, n_machines, execution_time))
-    file_iter.close()
-    return read_data_list
+                t_matrix[row] = np.array([int(column) for column in file.readline().split(' ')])
+            sets_read = sets_read + 1  # inkrementacja licznika wczytanych zestawów
+            ret.append(SchedulingData(name, n_jobs, n_machines, t_matrix))
+    file.close()
+    return ret
 
 
-def draw_gantt_plot(s_d: SchedulingData):
+def draw_gantt_plot(data: SchedulingData):
     pass
 
 
-def naive(s_d: SchedulingData):
+#  tworzy macierz harmonogramu zadań, szeregując zadania rosnąco dla każdej maszyny (najprostszy algorytm)
+def naive_scheduling(data: SchedulingData):
+    schedule = np.full(shape=(data.n_machines, data.n_jobs), fill_value=-1, dtype=int)
+    for machine in range(0, data.n_machines, 1):
+        for job in range(0, data.n_jobs, 1):
+            schedule[machine][job] = job
+    data.schedule = schedule
+
+
+def johnson_rule(data: SchedulingData):
     pass
 
 
-def johnson_rule(s_d: SchedulingData):
-    pass
-
-
-print(read_scheduling_data_file("neh.data.txt", 3))
+sched = read_scheduling_data_file("neh.data.txt", 3)`
+print(sched[0])
+naive_scheduling(sched[0])
+print(sched[0].schedule)
