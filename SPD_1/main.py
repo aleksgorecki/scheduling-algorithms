a8d9be3283fe5283f6dcdd16cd731992fd5486e3
Kacper Łukaszewicz <lukaszewiczkacper0@gmail.com>
2021-03-22 21:48:23 +0100

    menu

--- SPD_1/main.py
+++ SPD_1/main.py
@@ -1,9 +1,35 @@
 import scheduling_1
 
+ans = True
+while ans:
+    print("""
+    0.Exit/Quit
+    1.Bruteforce
+    2.Johnson rule
+    """)
+    ans = int(input("What would you like to do? "))
 
-dummy = scheduling_1.read_data_file("data/test.data.txt", 2)[1]
-if scheduling_1.verify_dataset(dummy):
-    print(scheduling_1.johnson_rule_multiple(dummy))
-    scheduling_1.gantt_chart(dummy)
-else:
-    print("Dataset is not in correct format!")
+    if ans == 0:
+        print("/n Goodbye")
+    elif ans == 1:
+        file_name ="data/" + str(input("Enter name of the data file: "))
+        dummy = scheduling_1.read_data_file(file_name, 2)[1]
+        if scheduling_1.verify_dataset(dummy):
+            print(scheduling_1.bruteforce(dummy))
+            scheduling_1.gantt_chart(dummy)
+        else:
+            print("Dataset is not in correct format!")
+    elif ans == 2:
+        file_name ="data/" + str(input("Enter name of the data file: "))
+        dummy = dummy = scheduling_1.read_data_file(file_name, 2)[1]
+        if scheduling_1.verify_dataset(dummy):
+            print(scheduling_1.johnson_rule_multiple(dummy))
+            scheduling_1.gantt_chart(dummy)
+        else:
+            print("Dataset is not in correct format!")
+    else:
+       print("\n Not valid choice try again")
+
+
+
+
