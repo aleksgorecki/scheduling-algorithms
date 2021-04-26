aabdd5f2e5c4ef9eee98bad04ff3650f39144f51
Aleksy <goreckialeksander9@gmail.com>
2021-04-27 00:49:34 +0200

    Katalog na kolejne zadanie

---
+++ SPD_4/timer.py
@@ -0,0 +1,14 @@
+import time
+
+
+class Timer:
+    def __init__(self):
+        self._start_time = None
+
+    def start(self):
+        self._start_time = time.perf_counter()
+
+    def stop(self):
+        elapsed_time = time.perf_counter() - self._start_time
+        self._start_time = None
+        return round(elapsed_time, 4)
